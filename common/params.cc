c5b336bb477646bbe327847ad893938db340aefc
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-23 10:46:26 -0700

    Use wheel speed in the onroad UI

--- common/params.cc
+++ common/params.cc
@@ -398,6 +398,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"WarningSoftVolume", PERSISTENT},
     {"WarningImmediateVolume", PERSISTENT},
     {"WheelIcon", PERSISTENT},
+    {"WheelSpeed", PERSISTENT},
 };
 
 } // namespace
